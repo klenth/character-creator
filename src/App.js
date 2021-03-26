8d93067748707aed195dd0fe07c799e1738bebe4
Kathryn Lenth <klenth@westminstercollege.edu>
2021-03-25 19:05:47 -0600

    More or less done (?)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Background from './components/Background';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <Background />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Background/Background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Background.module.css';
+
+const Background = () => {
+    return (<div class={styles.background}>
+
+    </div>);
+};
+
+export default Background;
